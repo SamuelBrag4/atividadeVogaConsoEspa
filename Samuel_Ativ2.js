e1d23f92da25c8becc9fcb4270256e40339aa31e
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-03-19 17:21:09 -0300

    desespero

--- Samuel_Ativ2.js
+++ Samuel_Ativ2.js
@@ -18,17 +18,38 @@ let consoantesTotais = [];
 
 let espacosTotais = [];
 
+let numeroConsoantes = [];
+
+let numeroVogais = []; 
+
 for(let i = 0; i < dividirTexto.length; i++){
     if(vogais.includes(dividirTexto[i])){
         vogaisTotais.push(dividirTexto[i]);
     }
 }
 
-for()
+for(let c = 0; c < dividirTexto.length; c++){
+    if(consoantes.includes(dividirTexto[c])){
+        consoantesTotais.push(dividirTexto[c]);
+    }
+}
+
+for(let b = 0; b < dividirTexto.length; b++){
+    if(espacosTotais.includes(dividirTexto[b])){
+        espacosTotaisTotais.push(dividirTexto[b]);
+    }
+}
+
+for(let d = 0; d < dividirTexto.length; d++){
+    if(numeroConsoantes.includes(dividirTexto[d])){
+        numeroConsoantes.push(dividirTexto[d]);
+    }
+}
+
 
 console.log("Texto inserido " + texto);
 console.log("Vogais encontradas: " + vogaisTotais);
 console.log("Consoantes encontradas: " + consoantesTotais);
-console.log("Total de vogais: "  );
-console.log("Total de consoantes: "  );
-console.log("Total de espaçõs: " + espacosTotais)
+console.log("Total de vogais: "   );
+console.log("Total de consoantes: " + numeroConsoantes.length);
+console.log("Total de espacos: " + espacosTotais.length);
